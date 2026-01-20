5a7eb4c30871b153208edfc23b096968f7b02520
Dmitry Churakov <churakov.dmitre2007@gmail.com>
2026-01-20 05:02:26 +0300

    first

--- django/gagarin/users/views.py
+++ django/gagarin/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from users.serializers import UserSerializer
+from rest_framework import status
 
-# Create your views here.
+class RegisterView(APIView):
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            
+            return Response({
+                "data": {
+                    "user": {
+                        "name": f"{user.last_name} {user.first_name} {user.patronymic}",
+                        "email": user.email
+                    },
+                    "code": 201,
+                    "message": "Пользователь создан"
+                }
+            }, status=status.HTTP_201_CREATED)
